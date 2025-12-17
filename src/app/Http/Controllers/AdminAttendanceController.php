38fe8bd9f01bf964a45bd1c5cfbf50df7ec4a0df
matsuyamashun <mashu107@icloud.com>
2025-12-17 13:48:45 +0900

    詳細修正画面終了

--- src/app/Http/Controllers/AdminAttendanceController.php
+++ src/app/Http/Controllers/AdminAttendanceController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DetailAttendanceRequest;
 use App\Models\Attendance;
+use App\Models\BreakTime;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class AdminAttendanceController extends Controller
 {
@@ -20,8 +23,42 @@ class AdminAttendanceController extends Controller
         return view('admin.attendance_list', compact('date', 'attendances', 'yesterday', 'tomorrow'));
     }
 
-    public function show()
+    public function show(Attendance $attendance)
     {
-        return view('admin.attendance_detail');
+        return view('admin.attendance_detail', compact('attendance'));
+    }
+
+    public function update(DetailAttendanceRequest $request, Attendance $attendance)
+    {
+        //全部成功で保存
+        DB::transaction(function () use ($request, $attendance) {
+
+            $date = $attendance->date;
+
+
+            //勤務時間更新
+            $attendance->update([
+                'clock_in' => Carbon::createFromFormat('Y-m-d H:i', $attendance->date . ' ' . $request->clock_in),
+                'clock_out' => Carbon::createFromFormat('Y-m-d H:i', $attendance->date . ' ' . $request->clock_out),
+                'note' => $request->note,
+            ]);
+
+            //休憩時間更新
+            if ($request->has('breaks')) {
+                foreach ($request->breaks as $break) {
+                    BreakTime::updateOrCreate(
+                        [
+                            'id' => $break['id'] ?? null,
+                        ],
+                        [
+                            'attendance_id' => $attendance->id,
+                            'start_time' => Carbon::createFromFormat('Y-m-d H:i', $attendance->date . ' ' . $break['start_time']),
+                            'end_time' => Carbon::createFromFormat('Y-m-d H:i', $attendance->date . ' ' . $break['end_time']),
+                        ]
+                    );
+                }
+            }
+        });
+        return redirect()->route('admin.attendance_staff', $attendance->user_id);
     }
 }
