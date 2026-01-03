d318440fd9b097f83ce3d24f3599b82795253c4e
matsuyamashun <mashu107@icloud.com>
2026-01-03 11:49:31 +0900

    テスト指摘箇所修正

--- src/tests/Feature/UpdateAttendanceDetailTest.php
+++ src/tests/Feature/UpdateAttendanceDetailTest.php
@@ -9,7 +9,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class UpdateAttendanceDatailTest extends TestCase
+class UpdateAttendanceDetailTest extends TestCase
 {
     use RefreshDatabase;
 
